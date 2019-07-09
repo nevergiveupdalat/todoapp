1e7e958deb15ae52c22834f70bd08f0614c07770
Hoang Anh Tuan <nevergiveupdalat@gmail.com>
2019-07-09 21:52:41 +0700

    add realm

--- TodoApp/AppDelegate.swift
+++ TodoApp/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let data = Data()
+        data.name = "Tuan"
+        data.age = 15
+        do{
+        let realm = try Realm()
+            try realm.write{
+                realm.add(data)
+            }
+        } catch
+        {
+            print("error realm : \(error)")
+        }
+        
         return true
     }
 
---
+++ TodoApp/Model/Data.swift
@@ -0,0 +1,15 @@
+//
+//  Data.swift
+//  TodoApp
+//
+//  Created by Tuan Hoang Anh on 7/9/19.
+//  Copyright © 2019 Tuan Hoang Anh. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+class Data: Object {
+    @objc dynamic var name: String = ""
+      @objc dynamic var age: Int = 10
+    
+}
